5a825079536cb1b71871d1caaf16e150bf4af3b4
Manfred Moser <manfred@simpligility.com>
2014-06-20 13:09:18 -0700

    adding some better logging and copying of figures for site publishing

--- makesite.sh
+++ makesite.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+echo "makesite starting"
+
+echo "makesite - clean -----------------------------------------------------"
 rm -rf site/ebook/*
 rm -rf site/book/*
-rm -rf site/translations/* 
+rm -rf site/translations/*
+rm -rf site/figures/* 
 
+echo "makesite - copy ------------------------------------------------------"
 cp -v tdm-book.en.epub site/ebook 
 cp -v tdm-book.en.mobi site/ebook
 cp -v tdm-book.en.pdf site/ebook
 cp -Rv en site/translations/
+cp -Rv figures site/
 cd site
+echo "makesite - rake ------------------------------------------------------"
 rake
+echo "makesite - jekyll ----------------------------------------------------"
 jekyll server
